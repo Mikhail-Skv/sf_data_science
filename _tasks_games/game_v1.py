354e9232fb546d08b4b5a54b97473d1bf25ca4d3
Mikhail-Skv <misha_skvortcov@mail.ru>
2022-10-04 23:27:02 +0300

    inserv version 3 and chenge version 1

--- _tasks_games/game_v1.py
+++ _tasks_games/game_v1.py
@@ -2,18 +2,28 @@ print('Hi')
 import numpy as np
 number = np.random.randint(1, 101)
 
-count = 0
 
-while True:
-    count+=1
-    predict_number = int(input("Угадай число от 1 до 100: "))
+def score_game() -> int:
     
-    if predict_number > number:
-        print('Число должно быть меньше    V')
+    count = 0
+
+    while True:
+        count+=1
+        predict_number = int(input("Угадай число от 1 до 100: "))
+    
+        if predict_number > number:
+            print('Число должно быть меньше    V')
         
-    elif predict_number < number:
-        print('Число должно быть больше    Л')
-    else:
-        print(f'Вы угадали! Это число = {number}, за {count} попыток')
-        break
-# 123 456
+        elif predict_number < number:
+            print('Число должно быть больше    ^')
+        
+        else:
+            print(f'Вы угадали! Это число = {number}, за {count} попыток')
+            break
+    
+    return(count)
+
+
+if __name__ == '__main__':      # не будет запускаться если подключить как библиотеку
+    #RUN
+    score_game() 
